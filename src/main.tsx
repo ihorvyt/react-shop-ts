3ebf872a9aa3434da29530303e3586536ab08db9
vlakk <lawashkalash@gmail.com>
2024-07-28 19:42:58 +0300

    started replacing js with ts

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,21 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from "react-router-dom"
-import App from "./App.jsx";
+import App from "./App.tsx";
 
 import {Provider} from "react-redux";
 import { store } from "./redux/store.js";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const rootEleme = document.getElementById('root')
+
+if (rootEleme) {
+    const root = ReactDOM.createRoot(rootEleme)
+
+    root.render(
         <BrowserRouter>
             <Provider store={store}>
                 <App/>
             </Provider>
         </BrowserRouter>
-)
+    )
+}
+
